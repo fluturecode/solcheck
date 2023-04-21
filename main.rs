5131ad69c0998910193920241d2328dffc869b06
Fluturecode <fluturecode@gmail.com>
2023-04-21 18:46:49 -0500

    moved main.rs to root

--- main.rs
+++ main.rs
@@ -9,9 +9,18 @@ use solana_program::{
 };
 
 // The entrypoint! macro defines the program's entry point. In this case, we are simply
-// calling a function named process_instruction and passing in the program ID, accounts, and instruction data.
+// calling a function named process_instruction and passing in the program ID, accounts,
+// and instruction data.
 entrypoint!(process_instruction);
 
+// The process_instruction function is where the bulk of the program logic will go. This
+// function takes in the
+// program ID, accounts, and instruction data, and returns a ProgramResult indicating
+// whether the instruction was processed successfully or not.
+
+// Within the process_instruction function, you would typically parse the instruction
+// data and perform any necessary checks or validations to ensure that the instruction
+// is valid and authorized.
 fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
